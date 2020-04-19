b65b86bff3da5000e7f7ebd8534a6d084f482e8c
unknown <62070088@kmitl.ac.th>
2020-04-19 11:47:42 +0700

    bug fixes

--- hangman.c
+++ hangman.c
@@ -3,10 +3,10 @@
 #include <time.h>
 #include <string.h>
 
-char progressBar[51];
+char *progressBar = NULL;
 char *screenPtr = NULL;
-char choosenWord[51];
-char hint[250];
+char *choosenWord = NULL;
+char *hint = NULL;
 char wordsArr[50][300];
 int points = 0;
 FILE *fptr;
@@ -25,17 +25,21 @@ void screenUpdate(char *outputText);
 void welcome_screen();
 int setDifficulty(int level);
 void getWordAndHint();
-char *createProgressBar(char *word);
+char *createProgressBar();
 char *updateProgressBar(char ch);
 int notInProgressBar(char ch);
 int correctCount(char ch);
 void playGame();
-int i = 0;
+void clearGame();
+int findSpace(char *word);
 
 int main(){
     //Main
     system("@cls||clear");
     screenPtr = malloc(600 * sizeof(char));
+    choosenWord = malloc(51 * sizeof(char));
+    progressBar = malloc(51 * sizeof(char));
+    hint = malloc(251 * sizeof(char));
     welcome_screen();
     getWordAndHint();
     sprintf(screenPtr, "%s \n\n Hint: %s\n\n     %s\n", bg[0], hint, createProgressBar(choosenWord));
@@ -46,8 +50,7 @@ int main(){
         char choice;
         scanf(" %c", &choice);
         if((int)choice == 'M' || (int)choice == 'm'){
-            fclose(fptr);
-            points = 0;
+            clearGame();
             main();
             break;}
         else if((int)choice == 'Y' || (int)choice == 'y'){
@@ -69,23 +72,24 @@ void screenUpdate(char *outputText){
     printf("%s\n", outputText);
 }
 
-char *createProgressBar(char *word){
-    strcpy(progressBar, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0");
-    for(i = 0; i < strlen(word); i++){
-        progressBar[i] = '-';
+char *createProgressBar(){
+    for(int i = 0; i < strlen(choosenWord); i++){
+        if((int)choosenWord[i] == ' '){progressBar[i] = ' ';}
+        else{progressBar[i] = '-';}
     }
+    progressBar[strlen(choosenWord)] = '\0';
     return progressBar;
 }
 
 char *updateProgressBar(char ch){
-    for(i = 0; i < strlen(choosenWord); i++){
+    for(int i = 0; i < strlen(choosenWord); i++){
         if((int)choosenWord[i] == ch){progressBar[i] = ch;}
     }
     return progressBar;
 }
 
 int notInProgressBar(char ch){
-    for(i = 0; i < strlen(progressBar); i++){
+    for(int i = 0; i < strlen(progressBar); i++){
         if((int)progressBar[i] == ch){return 0;}
     }
     return 1;
@@ -93,7 +97,7 @@ int notInProgressBar(char ch){
 
 int correctCount(char ch){
     int count = 0;
-    for(i = 0;i < strlen(choosenWord); i++){
+    for(int i = 0; i < strlen(choosenWord); i++){
         if((int)choosenWord[i] == ch){count++;}
     }
     return count;
@@ -104,8 +108,9 @@ void getWordAndHint(){
     char bufferRaw[300], bufferWord[51];
     int state = 0, num = 0;
     srand((unsigned) time(&t));
+    sprintf(choosenWord, "\0");
     strcpy(bufferRaw, wordsArr[rand() % (sizeof(wordsArr)/sizeof(wordsArr[0]))]);
-    for(i = 0; i <= strlen(bufferRaw); i++){
+    for(int i = 0; i <= strlen(bufferRaw); i++){
         if(state == 1){hint[num++] = bufferRaw[i];}
         else if((int)bufferRaw[i] != ';'){
             bufferWord[i] = bufferRaw[i];
@@ -123,15 +128,23 @@ int setDifficulty(int level){
     else if(level == 1){fptr = fopen("medium_words.txt", "r");}
     else if(level == 2){fptr = fopen("hard_words.txt", "r");}
     if(fptr == NULL){printf("Error opening file.");return -1;}
-    for(i = 0; i < 50; i++){
+    for(int i = 0; i < 50; i++){
         fgets(wordsArr[i], 300, fptr);
     }
     return 0;
 }
 
+int findSpace(char *word){
+    int count = 0;
+    for(int i = 0; i < strlen(word); i++){
+        if((int)word[i] == ' '){count++;}
+    }
+    return count;
+}
+
 void playGame(){
-    char temp;;
-    int state = 0, correctNum = 0, life = 7, count = 0;
+    char temp;
+    int state = 0, correctNum = findSpace(choosenWord), life = 7, count = 0;
     while(1){
         printf("Input a character: ");
         state = 0;
@@ -176,4 +189,17 @@ void welcome_screen(){
     else if ((int)diff == 'D' || (int)diff == 'd'){
         level = 2;}
     setDifficulty(level);
+}
+
+void clearGame(){
+    points = 0;
+    screenPtr = NULL;
+    choosenWord = NULL;
+    progressBar = NULL;
+    hint = NULL;
+    fclose(fptr);
+    system("@cls||clear");
+    for(int i = 0; i < 50; i++){
+        sprintf(wordsArr[i], "\0");
+    }
 }
